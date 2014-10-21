e288f242361af6b4692941901866e660476895e6
Christopher Brown <io@henrian.com>
2014-10-21 04:39:45 -0500

    Add simple elasticsearch loaders/extractors

---
+++ search.js
@@ -0,0 +1,29 @@
+/*jslint node: true */
+
+var elasticsearch = require('elasticsearch');
+var client = new elasticsearch.Client({
+  host: 'localhost:9200',
+  log: 'info'
+});
+
+
+client.search({
+  index: 'acl',
+  type: 'bib',
+  size: 20,
+  body: {
+    query: {
+      match: {
+        _all: 'twitter'
+      }
+    }
+  }
+}, function(err, response) {
+  if (err) throw err;
+  var result = response.hits;
+  console.log('Showing %d/%d results', result.hits.length, result.total);
+
+  var result_json = JSON.stringify(result, null, '  ');
+  console.log('search result: %s', result_json);
+  process.exit();
+});
