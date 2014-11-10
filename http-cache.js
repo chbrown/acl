3305a2fc2721a528fb54284846042534e5765c10
Christopher Brown <io@henrian.com>
2014-11-10 15:17:29 -0600

    Translate http-cache.js->ts

--- http-cache.js
+++ http-cache.js
@@ -1,40 +1,38 @@
-/*jslint node: true */
+/// <reference path="typings/tsd.d.ts" />
 var logger = require('loge');
 var redis = require('redis');
 var request = require('request');
-
-exports.get = function(url, callback) {
-  var redis_client = redis.createClient();
-
-  var done = function(err, result) {
-    redis_client.quit();
-    // escape redis's ugly exception-intercepting clutches
-    setImmediate(function() {
-      return callback(null, result);
+function get(url, callback) {
+    var redis_client = redis.createClient();
+    var done = function (err, result) {
+        redis_client.quit();
+        // escape redis's ugly exception-intercepting clutches
+        setImmediate(function () {
+            return callback(err, result);
+        });
+    };
+    redis_client.get(url, function (err, cached) {
+        if (err)
+            return done(err);
+        if (cached) {
+            logger.debug('Retrieved %s from cache', url);
+            return done(null, cached);
+        }
+        request.get(url, function (err, response, body) {
+            if (err)
+                return done(err);
+            if (response.statusCode != 200) {
+                logger.error('Non-200 response', response);
+            }
+            // expires in 6 hours
+            redis_client.setex(url, 6 * 60 * 60, body, function (err) {
+                if (err)
+                    return done(err);
+                logger.debug('Fetched %s from web', url);
+                done(null, body);
+            });
+        });
     });
-  };
-
-  redis_client.get(url, function(err, cached) {
-    if (err) return done(err);
-
-    if (cached) { //  && cached.length > 0
-      logger.debug('Found %s in cache', url);
-      return done(null, cached);
-    }
-
-    request.get(url, function(err, response, body) {
-      if (err) return done(err);
-      if (response.statusCode != 200) {
-        logger.error('Non-200 response', response);
-      }
-
-      // expires in 6 hours
-      redis_client.setex(url, 6 * 60*60, body, function(err) {
-        if (err) return done(err);
-
-        logger.debug('Retrieved %s from web', url);
-        done(null, body);
-      });
-    });
-  });
-};
+}
+exports.get = get;
+;
