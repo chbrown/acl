3f5578db48f7f2f90de93bc9eb4942bfe883afbb
Christopher Brown <io@henrian.com>
2015-03-06 15:40:27 -0600

    Move types -> type_declarations

--- type_declarations/index.d.ts
+++ type_declarations/index.d.ts
@@ -1,10 +1,13 @@
-/// <reference path="ext.d.ts" />
+/// <reference path="DefinitelyTyped.d.ts" />
 
 // my own external packages
 /// <reference path="domlike.d.ts" />
 /// <reference path="loge.d.ts" />
 /// <reference path="streaming.d.ts" />
 /// <reference path="tex.d.ts" />
+
+// common global types
+/// <reference path="common.d.ts" />
 
 // common global types
 /// <reference path="common.d.ts" />
