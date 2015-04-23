9887ce20cc36344b542d02e8e2223e682ea783a2
Christopher Brown <io@henrian.com>
2015-04-23 13:17:00 -0500

    Depend on type_declaration from tex library; add unorm to type_declarations/index

--- type_declarations/common.d.ts
+++ type_declarations/common.d.ts
@@ -1,3 +1,5 @@
-interface ErrorCallback { (err?: Error): void }
-interface StringMap { [index: string]: string; }
+interface ErrorCallback { (error?: Error): void; }
+// type ErrorCallback = (error?: Error) => void; // doesn't play nicely with previously declared ErrorCallback type
 interface ErrorResultCallback<T> { (err?: Error, result?: T): void }
+
+interface StringMap { [index: string]: string; }
--- type_declarations/index.d.ts
+++ type_declarations/index.d.ts
@@ -1,9 +1,14 @@
 /// <reference path="DefinitelyTyped.d.ts" />
 
+// self-declaring packages:
+/// <reference path="../node_modules/tex/tex.d.ts" />
+
 // my own external packages
 /// <reference path="domlike.d.ts" />
 /// <reference path="loge.d.ts" />
-/// <reference path="tex.d.ts" />
+
+// some external packages
+/// <reference path="unorm.d.ts" />
 
 // common global types
 /// <reference path="common.d.ts" />
